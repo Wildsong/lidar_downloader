821a938d34d432b0870f69dd8af22d4516a53260
Brian H Wilson <brian@wildsong.biz>
2020-03-20 17:54:23 -0700

    Ported to run in Windows with Visual Studio Code

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ I think I generated the CSV from the DOGAMI map viewer,
 or I might have done it in ArcGIS Pro? Struggling to remember now.
 Give me a few minutes and I'll get back to you.
 
+In Visual Studio Code I create an environment in Powershell window
+
+```bash
+python3 -m venv .venv
+python3 -m pip install -r requirements.txt
+```
--- download.py
+++ download.py
@@ -1,18 +1,43 @@
 #!env python
-from __future__ import print_function
 import sys, subprocess, os
+import requests
+import shutil
 
-fp = open("downloadableZips.csv")
-for f in fp.readlines():
-    (oid,url) = f.strip().split(',')
-    parts = url.split('/')
-    fname = parts[-1]
-    if (fname == 'LDQ_zip'):
-        continue
-    args = ["wget", "--quiet", "--no-check-certificate", url]
-    print(' '.join(args))
-    p = subprocess.check_output(args)
-    print(p)
-fp.close()
+if sys.version_info[:2] < (3, 6):
+    raise SystemExit("Come on, I need Python 3!")
+
+def download_file(url, local_filename):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True, verify=False) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw,f)
+    return
+
+# Count up how many files we are fetching
+count = 0
+files = []
+with open("downloadableZips.csv") as fp:
+    for f in fp.readlines():
+        (oid,url) = f.strip().split(',')
+        parts = url.split('/')
+        fname = parts[-1]
+        if (fname == 'LDQ_zip'): # Skip the column header line
+            continue
+        count += 1
+        files.append((url,fname))
+total = count
+
+# Fetch the files
+count = 0
+for (url,fname) in files:
+# This works great if your system knows how to run wget!! Not so great otherwise. :-)
+#    args = ["wget", "--quiet", "--no-check-certificate", url]
+#    print(' '.join(args))
+#    p = subprocess.check_output(args)
+    count += 1
+    print("Downloading %d/%d %s" % (count, total, fname), end="")
+    download_file(url, fname)
+    print()
 
 exit(-1)
